4efbc3d1c35af7700238caadfaf03c69f9812770
lisha7677 <lisha7677@gmail.com>
2016-11-18 18:23:53 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,37 +1,28 @@
 #1.Merges the training and the test sets to create one data set.
-#download zip file from url to working directory
+#download and unzip file from url to working directory
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",destfile = "dataset.zip")
-#unzip file to a folder in working directory
 unzip("dataset.zip")
 #set unzipped file to be the new working directory
 setwd("UCI HAR Dataset")
-#read 6 activity labels 1 WALKING 2 WALKING UPSTAIRS 3 WALKING DOWNSTAIRS 4 SITTING 5 STANDING 6 LAYING
+#read 6 activity labels and 561 features
 activityLabels<-read.table("activity_labels.txt")
-#read 561 features
 features<-read.table("features.txt",stringsAsFactors = FALSE)
-#read 9 test subjects
+#read 9 unique test subjects, test data, and test activity data
 subjectTest<-read.table("test/subject_test.txt")
-#read test data
 xTest<-read.table("test/X_test.txt")
-#read test activity data
 yTest<-read.table("test/y_test.txt")
-#read 21 train subjects
+#read 21 unique train subjects, train data, and train activity data
 subjectTrain<-read.table("train/subject_train.txt")
-#read train data
 xTrain<-read.table("train/X_train.txt")
-#read train activity data
 yTrain<-read.table("train/y_train.txt")
-#combine test data
-testData<-cbind(subjectTest,yTest,xTest)
-#combine train data
-trainData<-cbind(subjectTrain,yTrain,xTrain)
 #combine test and train data
+testData<-cbind(subjectTest,yTest,xTest)
+trainData<-cbind(subjectTrain,yTrain,xTrain)
 testTrain<-rbind(testData,trainData)
 colnames(testTrain)<-c("subject","activity",features[,2])
 #2.Extracts only the measurements on the mean and standard deviation for each measurement.
-#match pattern on mean() or std in features
+#match pattern on 'mean()' or 'std' in features, combine subject, activity, and selected features into one table.
 wantedFeatures<-grep("mean\\(\\)|std",features$V2,value = TRUE)
-#extract data with mean or std in features
 wantedData<-testTrain[,c("subject","activity",wantedFeatures)]
 #3.Uses descriptive activity names to name the activities in the data set.
 #turn activity vector into factor with levels and labels
